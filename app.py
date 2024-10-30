f471eae0989e5117cb53dc13a21e383af837698a
RafaSanCed <rafael.sanchez53099@gmail.com>
2024-10-29 19:07:12 -0600

    change structure3

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from app import create_app  
+from flask import Flask
+from app.routes import init_routes  # Importa `init_routes` desde la carpeta `app`
 
-app = create_app()
+app = Flask(__name__)
+
+# Configuración de la aplicación
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    # Otras configuraciones si es necesario
+)
+
+# Registra las rutas
+init_routes(app)
 
 if __name__ == '__main__':
     app.run(port=5000)
