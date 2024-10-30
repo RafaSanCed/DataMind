8487914765f0ea58d1a253b477dcea11e707a1a8
RafaSanCed <rafael.sanchez53099@gmail.com>
2024-10-29 18:46:21 -0600

    change structure

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         # Otras configuraciones aquí si es necesario
