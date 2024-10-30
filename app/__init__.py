2f1d48209976cd94cbd898ac2cc6df309e00d5f1
RafaSanCed <rafael.sanchez53099@gmail.com>
2024-10-29 18:53:29 -0600

    change structure2

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-
+    # Configuración de la aplicación
     app.config.from_mapping(
         SECRET_KEY='dev',
-        # Otras configuraciones aquí si es necesario
+        # Agrega otras configuraciones si es necesario
     )
 
     # Importar y registrar rutas
